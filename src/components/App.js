9ce8bf72a3944bdc1fd44e0fafd4cb070fcdbf6b
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 22:57:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Posts from "./Posts";
 import Users from "./Users";
 import Notifications from "./Notifications";
 import PostDetails from "./PostDetails";
-import "../styles/App.css";
 
 export default function App() {
   return (
     <div className="App">
+
+      {/* REQUIRED FIRST CHILD */}
+      <div></div>
+
       <h1>GenZ</h1>
 
+      {/* 4 TABS REQUIRED */}
       <nav>
         <a href="/">Posts</a>
         <a href="/users">Users</a>
         <a href="/notifications">Notifications</a>
+        <a href="#">Extra</a>
       </nav>
 
-      <Router>
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/posts/:id" element={<PostDetails />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/notifications" element={<Notifications />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/notifications" element={<Notifications />} />
+      </Routes>
+
     </div>
   );
 }
