7783cc042247f0fbe6008436a483ac7df16b945a
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 22:43:47 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 
-const Users = () => {
-  const [showPost, setShowPost] = useState(false);
+export default function Users() {
+  const [step, setStep] = useState(0);
 
   return (
     <>
-      <ul>
-        <li>Leanne</li>
-        <li>Ervin</li>
-        <li onClick={() => setShowPost(true)}>Clementine</li>
-      </ul>
-
-      {showPost && (
-        <div className="post">
-          <p>User Post</p>
-        </div>
+      {step === 0 && (
+        <ul>
+          <li onClick={() => setStep(1)}>User 1</li>
+          <li onClick={() => setStep(1)}>User 2</li>
+          <li onClick={() => setStep(1)}>User 3</li>
+        </ul>
       )}
+
+      {step === 1 && (
+        <ul>
+          <li>Post 1</li>
+          <li onClick={() => setStep(2)}>Post 2</li>
+        </ul>
+      )}
+
+      {step === 2 && <div className="post">User Post</div>}
     </>
   );
-};
-
-export default Users;
+}
