1a52436485ae7fe6f76faef35c668e153ab05465
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 22:42:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Posts from "./Posts";
 import Users from "./Users";
 import Notifications from "./Notifications";
 import PostDetails from "./PostDetails";
+import "../styles/App.css";
 
-const App = () => {
+export default function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        {/* FIRST CHILD (Cypress strict) */}
-        <nav>
-          <a href="/">Posts</a>
-          <a href="/users">Users</a>
-          <a href="/notifications">Notifications</a>
-        </nav>
+    <div className="App">
+      <h1>GenZ</h1>
 
-        <Switch>
-          <Route exact path="/" component={Posts} />
-          <Route exact path="/users" component={Users} />
-          <Route exact path="/notifications" component={Notifications} />
-          <Route path="/posts/:id" component={PostDetails} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+      <nav>
+        <a href="/">Posts</a>
+        <a href="/users">Users</a>
+        <a href="/notifications">Notifications</a>
+      </nav>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Posts />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/notifications" element={<Notifications />} />
+        </Routes>
+      </Router>
+    </div>
   );
-};
-
-export default App;
+}
