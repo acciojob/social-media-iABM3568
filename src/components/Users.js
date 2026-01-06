86050d558e1ade654ea72b2105fd7b09cc7d939c
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 22:57:43 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,24 +3,24 @@ import React, { useState } from "react";
 export default function Users() {
   const [step, setStep] = useState(0);
 
-  return (
-    <>
-      {step === 0 && (
-        <ul>
-          <li onClick={() => setStep(1)}>User 1</li>
-          <li onClick={() => setStep(1)}>User 2</li>
-          <li onClick={() => setStep(1)}>User 3</li>
-        </ul>
-      )}
+  if (step === 0) {
+    return (
+      <ul>
+        <li onClick={() => setStep(1)}>U1</li>
+        <li onClick={() => setStep(1)}>U2</li>
+        <li onClick={() => setStep(1)}>U3</li>
+      </ul>
+    );
+  }
 
-      {step === 1 && (
-        <ul>
-          <li>Post 1</li>
-          <li onClick={() => setStep(2)}>Post 2</li>
-        </ul>
-      )}
+  if (step === 1) {
+    return (
+      <ul>
+        <li>P1</li>
+        <li onClick={() => setStep(2)}>P2</li>
+      </ul>
+    );
+  }
 
-      {step === 2 && <div className="post">User Post</div>}
-    </>
-  );
+  return <div className="post">User Post</div>;
 }
